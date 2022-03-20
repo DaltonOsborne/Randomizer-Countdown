41fedb76f64bc5616156c0f16a8728f7f17eb5a2
DaltonOsborne <rainbowrocko24@hotmail.com>
2022-03-19 23:35:05 -0400

    movies added to listbox

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,28 @@ namespace Randomizer_Countdown
 {
     public partial class Form1 : Form
     {
-        public List<Movie> Movies = new List<Movie> { new Movie { Name = "This is cool" } };
+        public List<Movie> Movies = new List<Movie> { new Movie { Name = "Pokemon: The First Movie" },
+        new Movie {Name = "Pokemon:The Movie 2000"},
+        new Movie { Name = "Pokémon 3: The Movie - Spell of the Unown" },
+            new Movie { Name = "Pokémon 4Ever: Celebi - Voice of the Forest" },
+            new Movie { Name = "Pokémon Heroes: Latios and Latias" },
+            new Movie {Name = "Jirachi—Wish Maker" },
+            new Movie {Name = "Destiny Deoxys" },
+            new Movie {Name = "Lucario and the Mystery of Mew" },
+            new Movie {Name = "Pokemon Ranger and the Temple of the Sea" },
+            new Movie {Name = "The Rise of Darkrai" },
+            new Movie {Name = "Giratina and the Sky Warrior" },
+            new Movie {Name = "Arceus and the Jewel of Life" },
+            new Movie {Name = "Zoroark--Master of Illusions" },
+            new Movie {Name = "White--Victini and Zekrom" },
+            new Movie {Name = "Black--Victini and Reshiram" },
+            new Movie {Name = "Kyruem vs.the Sword of Justice" },
+            new Movie {Name = "Genesect and the Legend Awakened" },
+            new Movie {Name = "Diancie and the Coccoon of Destruction" },
+            new Movie {Name = "Hoopa and the Clash of Ages" },
+            new Movie {Name = "Volcanion and the MEchanical Marvel" },
+            new Movie {Name = "I Choose You!" },
+            new Movie {Name = "The Power of Us" },new Movie {Name = "Mewtwo Strikes Back:Evolution" },new Movie {Name = "Secrets of the Jungle" } };
         public Form1()
         {
             InitializeComponent();
@@ -29,5 +50,6 @@ namespace Randomizer_Countdown
 
             MessageBox.Show("The movie is: " + listBox1.SelectedItem.ToString());
         }
-    }
+        
+    } 
 }
